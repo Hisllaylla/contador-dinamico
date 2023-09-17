e77e8f8877228e3ce5d1ac076862e80ccb346a52
Hisllaylla <hisllayllak@gmail.com>
2023-09-16 22:17:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace projetinho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int numero))
+            {
+                for (int i = numero; i <= numero + 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Erro! Tente novamente.");
+            }
+        }
+    }
+}
